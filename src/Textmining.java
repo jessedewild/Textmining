6acc1bca5cbbd55faf1719b4f2c966e986d12cfc
jessedewild <jessedw@outlook.com>
2019-03-19 18:32:23 +0100

    Added final Strings for file paths.

--- src/Textmining.java
+++ src/Textmining.java
@@ -5,17 +5,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Textmining {
+    private final String INPUT_FILES_FOLDER = "input_files";
+    private final String SAVE_LOCATION = "output_from_input_files";
+
     private ArrayList<Publication> publications = new ArrayList<>(); // ArrayList of publications
 
     public void main() {
         try {
             // Add every Publication to an ArrayList
-            for (File file : TextFileUtilities.getFilesInFolder("input_files")) {
+            for (File file : TextFileUtilities.getFilesInFolder(INPUT_FILES_FOLDER)) {
                 publications.add(TextFileUtilities.parsePublicationInformation(file));
             }
 
             // Write the ArrayList of publications to a .txt file that has been given in the argInput
-            TextFileUtilities.writePublicationsToFile(publications, "output_from_files");
+            TextFileUtilities.writePublicationsToFile(publications, SAVE_LOCATION);
         } catch (TextFileUtitiesException | IOException e) {
             e.printStackTrace();
         }
